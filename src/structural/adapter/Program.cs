67f467dca7e1d0e0c082c78c142405d4027959d0
Crispin Tuballa <cristuballa@gmail.com>
2022-10-31 16:04:07 +1300

    Added adapter design pattern

---
+++ src/structural/adapter/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("**Adapter design pattern**");
+
+var adaptee = new Adaptee();
+ITarget target = new Adapter(adaptee);
+
+target.GetRequest();
