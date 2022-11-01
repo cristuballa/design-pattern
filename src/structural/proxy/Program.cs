b807c8431f8e10482cf1b7bbda39ebeb30acf1c4
Crispin Tuballa <cristuballa@gmail.com>
2022-11-02 11:29:05 +1300

    Added the client code class and call it from main

--- src/structural/proxy/Program.cs
+++ src/structural/proxy/Program.cs
@@ -1,6 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Proxy Design Pattern");
 
+            Client client = new Client();
+            
+            Console.WriteLine("Client: Executing the client code with a real subject:");
+            RealSubject realSubject = new RealSubject();
+            client.ClientCode(realSubject);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Executing the same client code with a proxy:");
+            Proxy proxy = new Proxy(realSubject);
+            client.ClientCode(proxy);
+
+
+
  // The Subject interface declares common operations for both RealSubject and
     // the Proxy. As long as the client works with RealSubject using this
     // interface, you'll be able to pass it a proxy instead of a real subject.
@@ -56,5 +70,22 @@ Console.WriteLine("Proxy Design Pattern");
         public void LogAccess()
         {
             Console.WriteLine("Proxy: Logging the time of request.");
+        }
+    }
+
+     public class Client
+    {
+        // The client code is supposed to work with all objects (both subjects
+        // and proxies) via the Subject interface in order to support both real
+        // subjects and proxies. In real life, however, clients mostly work with
+        // their real subjects directly. In this case, to implement the pattern
+        // more easily, you can extend your proxy from the real subject's class.
+        public void ClientCode(ISubject subject)
+        {
+            // ...
+            
+            subject.Request();
+            
+            // ...
         }
     }
