e52c6cb2ed97eb0f45a12c7038d1bec6d79b677a
Crispin Tuballa <cristuballa@gmail.com>
2022-11-02 10:42:14 +1300

    Separated facade classes

---
+++ src/structural/facade/Subsystem1.cs
@@ -0,0 +1,13 @@
+// See https://aka.ms/new-console-template for more information
+public class Subsystem1
+    {
+        public string operation1()
+        {
+            return "Subsystem1: Ready!\n";
+        }
+
+        public string operationN()
+        {
+            return "Subsystem1: Go!\n";
+        }
+    }
