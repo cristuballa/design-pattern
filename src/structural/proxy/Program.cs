c0e4b1711ce1d4c11164b7e98e4bcd1bbe57d10d
Crispin Tuballa <cristuballa@gmail.com>
2022-11-02 11:22:23 +1300

    Add ISubject, RealSubject on proxy

---
+++ src/structural/proxy/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Proxy Design Pattern");
+
+ // The Subject interface declares common operations for both RealSubject and
+    // the Proxy. As long as the client works with RealSubject using this
+    // interface, you'll be able to pass it a proxy instead of a real subject.
+    public interface ISubject
+    {
+        void Request();
+    }
+    
+    // The RealSubject contains some core business logic. Usually, RealSubjects
+    // are capable of doing some useful work which may also be very slow or
+    // sensitive - e.g. correcting input data. A Proxy can solve these issues
+    // without any changes to the RealSubject's code.
+    class RealSubject : ISubject
+    {
+        public void Request()
+        {
+            Console.WriteLine("RealSubject: Handling Request.");
+        }
+    }
